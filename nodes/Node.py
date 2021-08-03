d4043fb84952e86dfdf2cee6f664d935827b1927
root <root@EE-00001630.ericsson.se>
2021-08-03 15:36:35 +0300

    The Node.py has been changed

--- nodes/Node.py
+++ nodes/Node.py
@@ -1,4 +1,4 @@
-class Node:
+class Node(object):
     # Initialize default constructor, take as parameters a value from current node, and can add the next node (optional):
     def __init__(self, value, next_node=None):
         self.value = value
@@ -8,6 +8,11 @@ class Node:
     def get_value(self):
         return self.value
     
+    # Set value for a current node:
+    def set_value(self, new_value):
+        self.value = new_value
+
+    
     # Returns address of the next node:
     def get_next_node(self):
         return self.next_node
@@ -16,17 +21,18 @@ class Node:
     def set_next_node(self, next_node):
         self.next_node = next_node
 
-# Define a nodes
+# Define a nodes:
 node_a = Node(10)
 node_b = Node(20)
 node_c = Node(30)
 node_d = Node(50)
 
-# Connect nodes sequentially 
+# Connect nodes sequentially:
 node_a.set_next_node(node_b)
 node_b.set_next_node(node_c)
 node_c.set_next_node(node_d)
 
+# Get values for the next nodes:
 print(str(node_a.get_next_node().get_value()))
 print(str(node_b.get_next_node().get_value()))
 print(str(node_c.get_next_node().get_value()))
