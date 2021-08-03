07090882f97e9fcaa44d4c5600f3fcbe7d125ee7
root <root@EE-00001630.ericsson.se>
2021-08-03 15:54:43 +0300

    Nodes has been upgrated

--- nodes/Node.py
+++ nodes/Node.py
@@ -1,16 +1,16 @@
 class Node(object):
-    # Initialize default constructor, take as parameters a value from current node, and can add the next node (optional):
-    def __init__(self, value, next_node=None):
-        self.value = value
+    # Initialize default constructor, take as parameters a data from current node, and can add the next node (optional):
+    def __init__(self, data, next_node=None):
+        self.data = data
         self.next_node = next_node
 
-    # Returns value of current node:
-    def get_value(self):
-        return self.value
+    # Returns data of current node:
+    def get_data(self):
+        return self.data
     
-    # Set value for a current node:
-    def set_value(self, new_value):
-        self.value = new_value
+    # Set data for a current node:
+    def set_data(self, new_data):
+        self.data = new_data
 
     
     # Returns address of the next node:
@@ -20,6 +20,13 @@ class Node(object):
     # Set a new node:
     def set_next_node(self, next_node):
         self.next_node = next_node
+    
+    # Check if the next node is exist:
+    def has_next(self):
+        if self.next_node is None:
+            return False
+        else:
+            return True
 
 # Define a nodes:
 node_a = Node(10)
@@ -32,11 +39,13 @@ node_a.set_next_node(node_b)
 node_b.set_next_node(node_c)
 node_c.set_next_node(node_d)
 
-# Get values for the next nodes:
-print(str(node_a.get_next_node().get_value()))
-print(str(node_b.get_next_node().get_value()))
-print(str(node_c.get_next_node().get_value()))
+# Get datas for the next nodes:
+print(str(node_a.get_next_node().get_data()))
+print(str(node_b.get_next_node().get_data()))
+print(str(node_c.get_next_node().get_data()))
 
-
+# Check if next node is exist:
+print(node_a.has_next())
+print(node_d.has_next())
 
 
