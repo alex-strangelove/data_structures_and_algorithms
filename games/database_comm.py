ec522dac84b5d3f518286d26ff259db318b305a9
stangelove <sl@helios.yo>
2021-08-10 23:24:30 +0300

    First ver of game db was released, it does not work due development process.

---
+++ games/database_comm.py
@@ -0,0 +1,19 @@
+import os
+import sqlite3
+
+
+def db_connect(db_path):
+    con = sqlite3.connect(db_path)
+    return con
+
+def main():
+    # create a default path to connect to and create (if necessary) a database
+    # called 'database.sqlite3' in the same directory as this script
+    DEFAULT_PATH = os.path.join(os.path.dirname(__file__), 'database.sqlite3')
+    db_connect(db_path=DEFAULT_PATH)
+    
+
+
+
+
+main()
